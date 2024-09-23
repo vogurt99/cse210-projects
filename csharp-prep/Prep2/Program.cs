05a4b72b86f74b8837ec35ddc359cfd36cc26ea9
vogurt99 <vogurtsjrg@gmail.com>
2024-09-23 20:23:43 +0800

    Week 1 Prep Programs.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade and percentage? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write($"Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        
+        int listTotal = numbers.Sum(x => Convert.ToInt32(x));
+        Console.WriteLine($"The sum is: {listTotal}");
+
+        double listAverage = numbers.Average();
+        Console.WriteLine($"The average is: {listAverage}");
+
+        int largestNumber = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largestNumber)
+            largestNumber = number;
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
